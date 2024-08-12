1a4f6695d37f81088113b25de3b04f0d12235ea6
Aryan Hedaoo <aryanhedaoo@Aryans-MacBook-Air.local>
2024-08-12 16:21:24 +0530

    Output Successful

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import './App.css';
+import React,{useState} from 'react';
+
+function App() {
+
+  const [code,setCode]=useState('');
+  const [output,setOutput]=useState('');
+
+
+  const handleSubmit=async()=>{
+    const payload={
+      language:"cpp",
+      code
+    };
+    try{
+    const {data}=await axios.post("http://localhost:4000/run",payload)
+
+    setOutput(data.output)
+    console.log(output);
+  }catch(err){
+    console.log(err.response);
+  }
+  }
+
+
+  return (
+    <div className="App">
+     <h1>ONLINE CODE COMPILER</h1>
+     <textarea rows="20" cols="75" value={code} onChange={(e)=>{ 
+      setCode(e.target.value);
+
+     }}
+     ></textarea>
+     <br/>
+     <button onClick={handleSubmit}>Submit</button>
+     <p>{output}</p>
+    </div>
+  );
+}
+
+export default App;
